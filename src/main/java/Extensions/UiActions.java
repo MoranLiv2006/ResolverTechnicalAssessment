fe810442842ba8cc56db82d8419791eb4b77bfe9
Moran Liv <moran.liv2006@gmail.com>
2024-09-12 12:07:53 -0700

    Added more Ui actions for UiActions class

--- src/main/java/Extensions/UiActions.java
+++ src/main/java/Extensions/UiActions.java
@@ -2,11 +2,15 @@ package Extensions;
 
 import Utilities.Base;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class UiActions extends Base {
 
     public static void clickWebElement(WebElement webElement) {
+        //The elementToBeClickable expected wait condition is containing the expected wait condition of
+        // ExpectedConditions.visibilityOf, that means the element will be clickable ONLY once when that element
+        // will be visible and enable
         webDriverWait.until(ExpectedConditions.elementToBeClickable(webElement));
         webElement.click();
     }
@@ -14,5 +18,20 @@ public class UiActions extends Base {
     public static void writeTextToWebElement(WebElement webElement, String text) {
         webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
         webElement.sendKeys(text);
+    }
+
+    public static String getTextFromElement(WebElement webElement) {
+        webDriverWait.until(ExpectedConditions.visibilityOf(webElement));
+        return webElement.getText();
+    }
+
+    public static void scrollToElement(WebElement webElement) {
+        Actions actions = new Actions(webDriver);
+        actions.moveToElement(webElement).perform();
+    }
+
+    public static void chooseDropdownMenuOptionByValue(WebElement dropdownWebElement, WebElement optionToChoose) {
+        Actions mouse = new Actions(webDriver);
+        mouse.moveToElement(dropdownWebElement).click().moveToElement(optionToChoose).click().build().perform();
     }
 }
