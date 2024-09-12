0b655b8d531c5ac2ce26442ba339dc8f818fc760
Moran Liv <moran.liv2006@gmail.com>
2024-09-12 12:06:57 -0700

    Added initilization to all the page objects classes

--- src/main/java/Utilities/ManagePages.java
+++ src/main/java/Utilities/ManagePages.java
@@ -1,11 +1,15 @@
 package Utilities;
 
-import PageObjects.TestsPage;
+import PageObjects.*;
 import org.openqa.selenium.support.PageFactory;
 
 public class ManagePages extends Base {
 
     public static void initPages() {
-        testsPage = PageFactory.initElements(webDriver, TestsPage.class);
+        test1 = PageFactory.initElements(webDriver, Test1.class);
+        test2 = PageFactory.initElements(webDriver, Test2.class);
+        test3 = PageFactory.initElements(webDriver, Test3.class);
+        test4 = PageFactory.initElements(webDriver, Test4.class);
+        test5 = PageFactory.initElements(webDriver, Test5.class);
     }
 }
